d34e97c696c8a5f87df304fd49aa322dad107694
Kevin <kvalenzuela12345@gmail.com>
2018-12-05 22:45:38 +0000

    rpsFinished

--- js/script.js
+++ js/script.js
@@ -8,4 +8,51 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-
+$(function () {
+    $("#shoot").click(function () {
+        var Userchoice = $('#input').val();
+        var Computerchoice = '';
+        var x = Math.random();
+        var tie = false;
+        var uw = false;
+        
+        if(x<=0.33){
+            Computerchoice = 'rock';
+        }else if(x>0.33 && x<=.667){
+            Computerchoice = 'scissors';
+        }else if(x>.667 && x<=1){
+            Computerchoice = 'paper';
+        }else{
+            alert('error');
+        }
+        
+        if(Userchoice == Computerchoice){
+            tie = true;
+        }
+        else if(Userchoice == 'rock' && Computerchoice == 'scissors'){
+            uw = true;
+        }
+        else if(Userchoice == 'scissors' && Computerchoice == 'paper'){
+            uw = true;
+        }else if(Userchoice == 'paper' && Computerchoice == 'rock'){
+            uw = true;
+        }
+        var result = "";
+        if(uw) {
+            result = "You win you badass";
+        }else if(tie){
+            result = 'You tied';
+        }else{
+            result = 'You lost';
+        }
+        
+        if
+        $("#userChoice").text(Userchoice);
+        $('#computerChoice').text(Computerchoice);
+        $('#result').text(result);
+    })
+    
+    
+    
+    
+})
